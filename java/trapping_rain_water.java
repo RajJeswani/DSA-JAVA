923bb92020d8b1b748ff8e19f8ccffdd07fdb8bc
Raj Jeswani <rajjeswani43@gmail.com>
2026-09-22 21:31:56 +0530

    LeetCode: update java/trapping_rain_water.java

---
+++ java/trapping_rain_water.java
@@ -0,0 +1,14 @@
+// ======================================
+// LeetCode Problem: trapping rain water
+// Language: java
+// Link: https://leetcode.com/problems/trapping-rain-water/
+// Synced by: LinkCode
+// Date: 9/22/2026, 9:31:53 PM
+// ======================================
+
+
+class Solution {
+    public int trap(int[] height) {
+        
+    }
+}
