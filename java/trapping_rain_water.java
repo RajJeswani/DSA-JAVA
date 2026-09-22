6a1242e3af3274d5c41167a4a5cdfb303ce9fea8
Raj Jeswani <rajjeswani43@gmail.com>
2026-09-22 22:36:28 +0530

    LeetCode: update java/trapping_rain_water.java

--- java/trapping_rain_water.java
+++ java/trapping_rain_water.java
@@ -3,52 +3,33 @@
 // Language: java
 // Link: https://leetcode.com/problems/trapping-rain-water/
 // Synced by: LinkCode
-// Date: 9/22/2026, 9:32:19 PM
+// Date: 9/22/2026, 10:36:26 PM
 // ======================================
 
 
+//using two pointers approach
 class Solution {
     public int trap(int[] height) {
-        int n=height.length;
-     int[] left=new int[n];
-      left[0]=height[0];
-      for(int i=1;i<n;i++)
-      {
-        if(height[i]>left[i-1])
-        {
-          left[i]=height[i];
-        }
-        else
-        {
-          left[i]=left[i-1];
-        }
-      }
-      System.out.println(Arrays.toString(left));
 
-      int[] right=new int[n];
-      right[n-1]=height[n-1];
-      for(int i=n-2;i>=0;i--)
-      {
-        if(right[i+1]<height[i])
-        {
-          right[i]=height[i];
-        }
-        else
-        {
-          right[i]=right[i+1];
-        }
-      }
-      System.out.println(Arrays.toString(right));
+        int left = 0;
+        int right = height.length - 1;
 
-      int total=0;
-      for(int i=0;i<n;i++)
-      {
-        
-        int mw=Math.min(left[i],right[i]);
-        int tp=mw-height[i];
-        total+=tp;
-      }
-      return total;
-        
+        int water = 0;
+
+        int left_max = height[left], right_max = height[right];
+
+        while (left < right) {
+            if (left_max < right_max) {
+                left++;
+                left_max = Math.max(left_max, height[left]);
+                water += left_max - height[left];
+            } else {
+                right--;
+                right_max = Math.max(right_max, height[right]);
+                water += right_max - height[right];
+            }
+        }
+
+        return water;
     }
 }
