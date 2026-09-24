d42fb2cc78e43f0dd76fc705cc05cc86326bcf91
Raj Jeswani <rajjeswani43@gmail.com>
2026-09-25 00:31:01 +0530

    LeetCode: update java/minimum_size_subarray_sum.java

---
+++ java/minimum_size_subarray_sum.java
@@ -0,0 +1,26 @@
+// ======================================
+// LeetCode Problem: minimum size subarray sum
+// Language: java
+// Link: https://leetcode.com/problems/minimum-size-subarray-sum/
+// Synced by: LinkCode
+// Date: 9/25/2026, 12:30:57 AM
+// ======================================
+
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0, right = 0, sumOfCurrentWindow = 0;
+        int res = Integer.MAX_VALUE;
+
+        for(right = 0; right < nums.length; right++) {
+            sumOfCurrentWindow += nums[right];
+
+            while (sumOfCurrentWindow >= target) {
+                res = Math.min(res, right - left + 1);
+                sumOfCurrentWindow -= nums[left++];
+            }
+        }
+
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+}
